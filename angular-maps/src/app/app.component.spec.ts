cbe525168d436cdd0d83b42ebf5dee695923bdb7
Tristan-Gallo <gallo.tristan@iisgalvanimi.edu.it>
2023-05-08 09:14:06 +0000

    non si vede la mappa

--- angular-maps/src/app/app.component.spec.ts
+++ angular-maps/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'google-maps'`, () => {
+  it(`should have as title 'angular-maps'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('google-maps');
+    expect(app.title).toEqual('angular-maps');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('google-maps app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-maps app is running!');
   });
 });
--- google-maps/src/app/app.component.ts
+++ google-maps/src/app/app.component.ts
@@ -1,21 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
+import { GEOJSON, GeoFeatureCollection } from './models/geojson.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+
+export class AppComponent implements AfterViewInit {
   title = 'server mappe';
   //Aggiungiamo latitudine e longitudine di un luogo
-  center : any;
-  position : any;
-  label :string;
-  constructor()
-  {
-    this.center={lat: 45.506738, lng: 9.190766};
-    this.position = this.center;
-    this.label = "ciao";
+  center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
+  geoJsonObject! : GeoFeatureCollection;
+  fillColor: string = "#FF0000";  //Colore delle zone catastali
+  styleFunc = (feature:any) =>{
+    console.log(feature.i.id)
+    let newColor = "#FF0000"; //RED
+    if(feature.i.id == 0) newColor = "#00FF00"; //GREEN
+    else newColor = "#0000FF"; //BLUE
+    return ({
+      clickable: false,
+      fillColor: newColor,
+      strokeWeight: 1
+    });
   }
+ 
+  markerList! : google.maps.MarkerOptions[];
+  zoom = 8;
+  @ViewChild('mapRef') mapRef!: GoogleMap;
+  ngAfterViewInit() {
+  this.mapRef.data.addGeoJson(this.geoJsonObject);
+  this.mapRef.data.setStyle(this.styleFunc);
+  
+}
+  constructor() {
 
+    //Questi dati dovremmo scaricarli dal server, per ora li abbiamo copiati nel file     gojson.model.ts
+    this.geoJsonObject = GEOJSON;  
+    console.log(this.geoJsonObject); //stampo l'oggetto geoJsonObject sulla console
+    
+
+  }
 }
